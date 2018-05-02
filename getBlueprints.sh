3f04111219a7eb7561f84439884b5656d17a35ca
John Masci <JMasci@Grid-JMasci-MBP.local>
2018-05-01 20:29:43 -0400

    added directory for blueprints

--- getBlueprints.sh
+++ getBlueprints.sh
@@ -9,15 +9,18 @@
 user=$1
 token=$2
 url=$3
+folder=blueprints.$(date +%F_%R)
 
 echo "Backing Up Blueprints Entitled to User " $1 ":"
 echo
 echo
 
+mkdir $folder
+
 while read id; do
 name=$(curl -ks --user $user:$token https://execdemo5.skygrid.cloud/api/blueprints/$id | jq -r '.results.name')
 echo $name
-curl -ks --user $user:$token https://$url/api/blueprints/$id | jq '.results | { id, name, version, tags, description, shortDescription, externalLink, imageLink, yml, visibility, params, entitlementType, entitledUsers, entitledUserGroups }' > "HCP-$name.$(date +%F_%R).txt"
+curl -ks --user $user:$token https://$url/api/blueprints/$id | jq '.results | { id, name, version, tags, description, shortDescription, externalLink, imageLink, yml, visibility, params, entitlementType, entitledUsers, entitledUserGroups }' > $folder/"HCP-$name.$(date +%F_%R).txt"
 done <blueprintList.txt
 
 rm blueprintList.txt
